e6dacd78183399d7fb6ac9c63492b0890c2ee517
zenithdreamer <49715907+zenithdreamer@users.noreply.github.com>
2024-08-10 10:00:57 +0700

    style: format

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -4,10 +4,8 @@ export * from "@prisma/client";
 
 const globalPrisma = globalThis as { prisma?: PrismaClient };
 
- 
 export const prisma =
   globalPrisma.prisma ??
-   
   new PrismaClient({
     log:
       // eslint-disable-next-line no-restricted-properties
