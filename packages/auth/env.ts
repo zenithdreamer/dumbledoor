2780abac61d03faa4f6a68a5319795e36615d50d
zenithdreamer <49715907+zenithdreamer@users.noreply.github.com>
2024-08-13 10:27:31 +0700

    test

--- packages/auth/env.ts
+++ packages/auth/env.ts
@@ -16,11 +16,26 @@ export const env = createEnv({
     ACCESS_SERVICE_URL: z.string().optional(),
     USER_SERVICE_URL: z.string().optional(),
     CARD_SERVICE_URL: z.string().optional(),
+    DOOR_SERVICE_URL: z.string().optional(),
     INTERNAL_API_SECRET: z.string().min(1),
     NODE_ENV: z.enum(["development", "production"]).optional(),
   },
   client: {},
-  experimental__runtimeEnv: {},
+  experimental__runtimeEnv: {
+    ACCESS_SERVICE_PORT: z
+      .string()
+      .transform((val) => parseInt(val, 10))
+      .default("4001"),
+    USER_SERVICE_PORT: z
+      .string()
+      .transform((val) => parseInt(val, 10))
+      .default("4000"),
+    ACCESS_SERVICE_URL: z.string().optional(),
+    USER_SERVICE_URL: z.string().optional(),
+    CARD_SERVICE_URL: z.string().optional(),
+    DOOR_SERVICE_URL: z.string().optional(),
+    INTERNAL_API_SECRET: z.string().min(1),
+  },
   skipValidation:
     !!process.env.CI || process.env.npm_lifecycle_event === "lint",
 });
