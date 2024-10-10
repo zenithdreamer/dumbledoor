7c6e773a71f1868201f35d2492072ccef18c40c5
zenithdreamer <49715907+zenithdreamer@users.noreply.github.com>
2024-10-10 22:53:43 +0700

    fix: invalid env

--- packages/auth/env.ts
+++ packages/auth/env.ts
@@ -22,19 +22,19 @@ export const env = createEnv({
   },
   client: {},
   experimental__runtimeEnv: {
-    ACCESS_SERVICE_PORT: z
-      .string()
-      .transform((val) => parseInt(val, 10))
-      .default("4001"),
-    USER_SERVICE_PORT: z
-      .string()
-      .transform((val) => parseInt(val, 10))
-      .default("4000"),
-    ACCESS_SERVICE_URL: z.string().optional(),
-    USER_SERVICE_URL: z.string().optional(),
-    CARD_SERVICE_URL: z.string().optional(),
-    DOOR_SERVICE_URL: z.string().optional(),
-    INTERNAL_API_SECRET: z.string().min(1),
+    // ACCESS_SERVICE_PORT: z
+    //   .string()
+    //   .transform((val) => parseInt(val, 10))
+    //   .default("4001"),
+    // USER_SERVICE_PORT: z
+    //   .string()
+    //   .transform((val) => parseInt(val, 10))
+    //   .default("4000"),
+    // ACCESS_SERVICE_URL: z.string().optional(),
+    // USER_SERVICE_URL: z.string().optional(),
+    // CARD_SERVICE_URL: z.string().optional(),
+    // DOOR_SERVICE_URL: z.string().optional(),
+    // INTERNAL_API_SECRET: z.string().min(1),
   },
   skipValidation:
     !!process.env.CI || process.env.npm_lifecycle_event === "lint",
